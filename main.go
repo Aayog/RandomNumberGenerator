e8a912796462b1a4aeca9b98d1c9cc3229f9a080
Aayog Koirala <koirala@miamioh.edu>
2024-01-23 21:22:23 +0000

    Invalid DateTime

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func main() {
 		increment:  1013904223,
 		seed:       1,
 	}
-	prev := getRandomInt(LCG, generator, 1, 1, 10)
+	prev := getRandomInt(LCG, generator, 1000, 1, 10)
 	for i := 0; i < 100; i++ {
 
 		fmt.Println(prev)
